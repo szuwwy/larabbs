87fb369b08dcf421d15cacf0fb5ba9505a5849be
wwy <120046911@qq.com>
2019-02-12 09:43:22 +0000

    add captcha

--- routes/api.php
+++ routes/api.php
@@ -25,7 +25,7 @@ $api->version('v1', [
      $api->group([
         'middleware' => 'api.throttle',
         'limit' => config('api.rate_limits.sign.limit'),
-        'expires' => config('api.rate_limits.sign.expires'),,
+        'expires' => config('api.rate_limits.sign.expires'),
     ], function($api) {
         // 短信验证码
         $api->post('verificationCodes', 'VerificationCodesController@store')
@@ -34,6 +34,9 @@ $api->version('v1', [
         $api->post('users', 'UsersController@store')
             ->name('api.users.store');
     });
+    // 图片验证码
+    $api->post('captchas', 'CaptchasController@store')
+        ->name('api.captchas.store');
     // 短信验证码
     // $api->post('verificationCodes', 'VerificationCodesController@store')
     //     ->name('api.verificationCodes.store');
